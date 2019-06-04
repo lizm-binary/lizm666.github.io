01817b5e457df2a59781c53f58cbc42fed3dfa60
lizm <lizm@servyou.com.cn>
2019-06-04 14:18:48 +0800

    add music player

--- assets/js/music.js
+++ assets/js/music.js
@@ -14,18 +14,23 @@ if (musicTag) {
     musicTag.parentNode.insertBefore(frame, musicTag);
     frame.frameBorder = 0;
     frame.width = '100%';
-    frame.height = 80;
-    //frame.style.marginTop = '-19px';
-    frame.style.marginBottom = '10px';
+    frame.height = 100;
+    frame.style.marginBottom = '20px';
     frame.style.display = 'none';
-    frame.src = '/assets/music/' + musicTag.value;
+    var musicInfo = musicTag.value.split(',');
+    frame.src = '/assets/music/' + musicInfo[0];
     frame.onload = function () {
         var css = frame.contentWindow.document.createElement('link');
         css.rel = 'stylesheet';
         css.href = '/assets/css/music.css';
         var head = frame.contentWindow.document.getElementsByTagName('head')[0];
         head.appendChild(css);
-        frame.contentWindow.document.getElementsByTagName('video')[0].loop = true;
+        var video = frame.contentWindow.document.getElementsByTagName('video')[0];
+        video.loop = true;
+        var span = document.createElement('span');
+        span.innerText = '♬：' + (musicInfo[1] || '未知');
+        span.style.marginLeft = '20px';
+        video.parentNode.insertBefore(span, video);
         frame.style.display = 'block';
     };
 }
